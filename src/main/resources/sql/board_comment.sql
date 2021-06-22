45c65c11d5b70ff3c8246d45f825c0d7074af18c
sharonkim1010 <tifhs8989@naver.com>
2021-06-22 22:18:04 +0900

    완성2

--- src/main/resources/sql/board_comment.sql
+++ src/main/resources/sql/board_comment.sql
@@ -4,5 +4,6 @@ user_id VARCHAR2(30) not null,
 comment_content VARCHAR2(100) not null,
 comment_id NUMBER not null,
 comment_layer NUMBER not null,
-comment_date date default sysdate
+comment_date date default sysdate,
+constraint fk_comment foreign key(board_id) references board(board_id) on delete cascade
 );
