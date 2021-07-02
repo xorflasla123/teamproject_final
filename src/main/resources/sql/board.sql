293a28c24fac2815ec7215ed8687844dbc467d41
leelim <xorflasla123@gmail.com>
2021-07-02 18:35:06 +0900

    수정

--- src/main/resources/sql/board.sql
+++ src/main/resources/sql/board.sql
@@ -1,11 +1,12 @@
 create table board(
-board_id NUMBER primary key, 
-board_local VARCHAR2(20) not null,
-user_id VARCHAR2(20) not null,
-board_title VARCHAR2(4000) not null,
-board_content VARCHAR2(4000),
-board_day date default sysdate,
-board_picture VARCHAR2(4000),
-board_hit NUMBER default 0
+	board_id NUMBER primary key, 
+	board_local VARCHAR2(20) not null,
+	user_id VARCHAR2(20) not null,
+	board_title VARCHAR2(4000) not null,
+	board_content VARCHAR2(4000),
+	board_day date default sysdate,
+	board_picture VARCHAR2(4000),
+	board_hit NUMBER default 0
 );
+
 create sequence board_seq;
